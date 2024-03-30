2699aae5abf3e73026fe38e891a960d229e641a8
semickolon <a9iqln3u@anonaddy.me>
2024-03-31 03:49:02 +0800

    nix: Update Nickel to 1.5.0

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     naersk.url = "github:nix-community/naersk";
-    nickel.url = "github:tweag/nickel/1.4.1";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
@@ -12,6 +12,7 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        pkgs-unstable = import inputs.nixpkgs-unstable { inherit system; };
         naersk = pkgs.callPackage inputs.naersk {};
         wchisp = naersk.buildPackage rec {
           pname = "wchisp";
@@ -23,15 +24,18 @@
             hash = "sha256-Ju2DBv3R4O48o8Fk/AFXOBIsvGMK9hJ8Ogxk47f7gcU=";
           };
         };
-        nickel = inputs.nickel.packages.${system}.default;
         packages = with pkgs; [
           sdcc
-          topiary
-          nickel
+          pkgs-unstable.nickel
+          pkgs-unstable.nls
+          pkgs-unstable.topiary
           meson
           ninja
           python311
         ];
+        shellHook = ''
+          alias fak="python fak.py"
+        '';
       in
       {
         devShells = {
